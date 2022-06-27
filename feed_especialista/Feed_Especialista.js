2d8ba0e4584ac8f98f6afc28d4bbd3c6722d3e7a
Roberta da Silva <89100877+RobertadaSilva@users.noreply.github.com>
2022-06-27 13:04:06 -0300

    Add files via upload

---
+++ feed_especialista/Feed_Especialista.js
@@ -0,0 +1,25 @@
+
+function visivel_um() {
+    var mudar = document.querySelector('.filtro_opcoes_um');
+    mudar.style.display = 'block';
+}
+function invisivel_um() {
+    var mudar = document.querySelector('.filtro_opcoes_um');
+    mudar.style.display = 'none';
+}
+function visivel_dois() {
+    var mudar = document.querySelector('.filtro_opcoes_dois');
+    mudar.style.display = 'block';
+}
+function invisivel_dois() {
+    var mudar = document.querySelector('.filtro_opcoes_dois');
+    mudar.style.display = 'none';
+}
+function visivel_tres() {
+    var mudar = document.querySelector('.filtro_opcoes_tres');
+    mudar.style.display = 'block';
+}
+function invisivel_tres() {
+    var mudar = document.querySelector('.filtro_opcoes_tres');
+    mudar.style.display = 'none';
+}
